ceb5a2966724e1e90865cd76361b4977d7d688a7
Karl Markus Wahlberg <skmw@greeny.cs.tlu.ee>
2015-09-14 13:13:29 +0300

    created new pages: header, footer, menu, home

---
+++ header.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Siia tuleb nimi hiljem</title>
+	</head>
+	
+	<body>
+	
+	<?php
+		//laeme menu.php faili sisu. require_once - võetakse ühe korra.
+		require_once("menu.php");
+		
+	?>
