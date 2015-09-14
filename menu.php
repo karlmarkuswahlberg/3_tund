75614d5ee00e69a6369857091d4d6f5fa81441ee
Karl Markus Wahlberg <skmw@greeny.cs.tlu.ee>
2015-09-14 13:48:29 +0300

    added navigation menu names for home and login

--- menu.php
+++ menu.php
@@ -1,12 +1,24 @@
 <h2>Menüü</h2>
 
+<?php echo $page_file_name; ?>
 <ul>
-	<li>
-		<a href="home.php">Avaleht</a>
-	</li>
+
+	<?php 
+	//ükskõik mis lehe puhul näitan linki aga kui on 
+	//home leht, sisi lihtsalt nime
+	if($page_file_name != "home.php") { ?>
+	<li><a href="home.php">Avaleht</a></li>
+	<?php } else { ?>
+		<li>Avaleht</li>
+	<?php } ?>
 	
-	<li>
-		<a href="login.php">Logi sisse!</a>
-	</li>
+	
+	<?php
+	if($page_file_name != "login.php"){
+		echo '<li><a href="login.php">Logi sisse!</a></li>';
+	}else{
+		echo '<li>Logi sisse!</li>';
+	}
+	?>
 	
 </ul>
